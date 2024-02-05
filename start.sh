4a52a4b695904c09d7931c1e5cfc66e3cf10c9e4
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-02-05 20:50:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Candypop678/MINNAL-T_ADV-V22.git /MINNAL-T_ADV-V2
+  git clone https://github.com/Kushalhk/TG_BOTZ.git /TG_BOTZ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MINNAL-T_ADV-V2
+  git clone $UPSTREAM_REPO /TG_BOTZ
 fi
-cd /MINNAL-T_ADV-V2
+cd /TG_BOTZ
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
